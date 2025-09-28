c9ad07f1c7d8df9049c84519076b90626a1498ae
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-09-28 13:13:06 +0800

    Update main.tf - download ssm agent

--- main.tf
+++ main.tf
@@ -203,7 +203,7 @@ resource "aws_instance" "rhel" {
     usermod -aG wheel ssm-user2
     
     # Install SSM agent
-    yum install -y amazon-ssm-agent
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
     systemctl enable amazon-ssm-agent
     systemctl start amazon-ssm-agent
     
